cf0671ef391b979410702712f8a6ccf1fecd0f77
shaqz12 <1sters06@solent.ac.uk>
2017-04-27 11:43:05 +0100

    New changes has been made

--- app/src/main/java/com/example/a1sters06/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1sters06/mapping/HelloMap.java
@@ -1,11 +1,18 @@
 package com.example.a1sters06.mapping;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
@@ -45,7 +52,8 @@ public class HelloMap extends Activity
 
         mv.setBuiltInZoomControls(false);
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(51.582403,-0.111633));
+        mv.getController().setCenter(new GeoPoint(50.9, -1.4));
+
 
         markerGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>()
         {
@@ -55,10 +63,10 @@ public class HelloMap extends Activity
                 return true;
             }
             public boolean onItemSingleTapUp(int i, OverlayItem item)
-             {
+            {
                 Toast.makeText(HelloMap.this, item.getSnippet(), Toast.LENGTH_LONG).show();
-                 return true;
-             }
+                return true;
+            }
         };
 
         items = new ItemizedIconOverlay<OverlayItem>(this,new ArrayList<OverlayItem>(),null);
@@ -70,7 +78,8 @@ public class HelloMap extends Activity
 
         ArrayList<Overlay> overlay = new ArrayList<Overlay>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("poi.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath()+
+                    "/poi.txt"));
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] components = line.split(",");
@@ -83,7 +92,26 @@ public class HelloMap extends Activity
         }
         catch(IOException e)
         {
-
+            new AlertDialog.Builder(this).setMessage(e.toString()).setPositiveButton("OK", null).show();
         }
     }
+
+    /*
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,HelloMap.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+    */
 }
